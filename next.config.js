de6c6c7c87816b58c6d8cc34bc7d94ed0d182589
Ivica Batinic <ivica.batinic@infinum.hr>
2021-05-23 00:01:10 +0200

    Enable next.js eslint

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const withReactSvg = require('next-react-svg');
 
+const nextConfig = {
+	experimental: {
+		eslint: true,
+	},
+};
+
 module.exports = withPlugins([
 	[
 		withReactSvg,
@@ -9,4 +15,5 @@ module.exports = withPlugins([
 			include: path.resolve(__dirname, 'src/assets/icons'),
 		},
 	],
+	nextConfig,
 ]);
