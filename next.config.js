265c0bddd9fe85c99596da44d7a372cd6fa57143
Ivica Batinic <ivica.batinic@infinum.hr>
2021-06-22 18:27:21 +0200

    Update libs

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withPlugins = require('next-compose-plugins');
 const withReactSvg = require('next-react-svg');
 
 const nextConfig = {
-	future: {
-		webpack5: true,
-	},
 	experimental: {
 		eslint: true,
 	},
